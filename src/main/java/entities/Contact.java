defb2d11a0bc864cac07795d6c00fbab66a4019c
joseph mbimbi <joseph.mbimbi@gmail.com>
2017-08-20 20:35:27 +0200

    une première version un peu crade du back

---
+++ src/main/java/entities/Contact.java
@@ -0,0 +1,95 @@
+package entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@NamedQueries({ @NamedQuery(name = "getContactsByUserId", query = "from Contact c where c.userId = :userId"),
+		@NamedQuery(name = "getContacts", query = "from Contact") })
+@Entity
+@Table(name = "contacts")
+public class Contact {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	@JsonIgnore
+	private Integer ID;
+
+	@Column(name = "nom")
+	private String nom;
+
+	@Column(name = "prenom")
+	private String prenom;
+
+	@Column(name = "mail")
+	private String mail;
+
+	@Column(name = "telephone")
+	private String telephone;
+
+	@Column(name = "user_id")
+	private Integer userId;
+
+	public Integer getID() {
+		return ID;
+	}
+
+	public void setID(Integer iD) {
+		ID = iD;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact [ID=" + ID + ", nom=" + nom + ", prenom=" + prenom + ", mail=" + mail + ", telephone="
+				+ telephone + ", userId=" + userId + "]";
+	}
+
+}
